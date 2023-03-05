6d2f49c06fc943d2e076e02eca77c1cf638e0c26
syafikadin <syafik.adin222@gmail.com>
2023-03-05 21:57:29 +0700

    create edit student

---
+++ resources/views/admin/students/edit.blade.php
@@ -0,0 +1,13 @@
+@extends('admin.layouts.main')
+
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Edit Student</h1>
+    </div>
+
+    <div class="col-lg-6">
+        <form action="">
+            
+        </form>
+    </div>
+@endsection
