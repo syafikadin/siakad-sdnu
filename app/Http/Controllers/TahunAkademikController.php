25eea09ae6dc826f1dba52fde903016c44ae94ef
syafikadin <syafik.adin222@gmail.com>
2023-03-09 10:40:33 +0700

    make tahun akademik model

---
+++ app/Http/Controllers/TahunAkademikController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TahunAkademik;
+use App\Http\Requests\StoreTahunAkademikRequest;
+use App\Http\Requests\UpdateTahunAkademikRequest;
+
+class TahunAkademikController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTahunAkademikRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTahunAkademikRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TahunAkademik  $tahunAkademik
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TahunAkademik $tahunAkademik)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TahunAkademik  $tahunAkademik
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TahunAkademik $tahunAkademik)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTahunAkademikRequest  $request
+     * @param  \App\Models\TahunAkademik  $tahunAkademik
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTahunAkademikRequest $request, TahunAkademik $tahunAkademik)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TahunAkademik  $tahunAkademik
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TahunAkademik $tahunAkademik)
+    {
+        //
+    }
+}
