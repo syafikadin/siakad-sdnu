fb9159b5dbbe6b2b3be83e0d8cf684669247ddb8
syafikadin <syafik.adin222@gmail.com>
2023-03-09 10:44:37 +0700

    add teacher view route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\MataPelajaranController;
+use App\Http\Controllers\RaportController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TeacherController;
+use App\Models\MataPelajaran;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,8 +34,14 @@ Route::get('/dashboard', function () {
     return view('dashboard.index');
 })->middleware('user');
 
+// Route::resource('/dashboard/jadwal');
+
 Route::get('/admin', function () {
     return view('admin.index');
 })->middleware('admin');
 
 Route::resource('/admin/students', StudentController::class);
+
+Route::resource('/admin/teachers', TeacherController::class);
+
+Route::resource('/admin/mapel', MataPelajaranController::class);
