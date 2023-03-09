61549fe0f399adffcdc4bdfdccde0055bf775ec2
syafikadin <syafik.adin222@gmail.com>
2023-03-09 10:45:19 +0700

    add raport view

---
+++ resources/views/dashboard/raports/index.blade.php
@@ -0,0 +1,41 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Tabel Raport</h1>
+    </div>
+
+    <div class="table-responsive col-lg-8">
+          <table class="table table-striped table-sm mt-3">
+            <thead>
+              <tr class="text-center">
+                <th scope="col">No</th>
+                <th scope="col">NIS</th>
+                <th scope="col">Nama</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {{-- @foreach ($students as $student)    
+              <tr>
+                <td class="text-center">{{ $loop->iteration }}</td>
+                <td>{{ $student->nis }}</td>
+                <td>{{ $student->nama }}</td>
+                <td class="text-center">
+                  <a href="/admin/students/{{ $student->username }}" class="badge bg-info"><span data-feather="info" class="align-text-bottom"></span></a>
+                  <a href="/admin/students/{{ $student->username }}/edit" class="badge bg-warning"><span data-feather="edit" class="align-text-bottom"></span></a>
+                  
+                  <form action="/admin/students/{{ $student->username }}" method="post" class="d-inline">
+                    @method('delete')
+                    @csrf
+                    <button class="badge bg-danger border-0" onclick="return confirm('Are You Sure?')">
+                      <span data-feather="trash-2" class="align-text-bottom"></span>
+                    </button>
+                  </form>
+                </td>
+              </tr>
+              @endforeach --}}
+            </tbody>
+          </table>
+        </div>
+@endsection
